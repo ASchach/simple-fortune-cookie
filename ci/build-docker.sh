ea426db86a55dac221b165c1d9c640261f251f94
Samuel Njri <sanjr19@student.sdu.dk>
2021-08-22 16:22:00 +0000

    add files for sha v1.3

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}simple-fortune-cookie-backend:latest" -t "${DockerRepo}simple-fortune-cookie-backend:$Tag" ./backend
-docker build -t "${DockerRepo}simple-fortune-cookie-frontend:latest" -t "${DockerRepo}simple-fortune-cookie-frontend:$Tag" ./frontend
+SHORTHASH="$(git rev-parse --short HEAD)"
+docker tag "$IMAGE_NAME" "${DockerRepo}simple-fortune-cookie-backend:$SHORTHASH"
+docker build -t "${DockerRepo}simple-fortune-cookie-backend:latest" -t "${DockerRepo}simple-fortune-cookie-backend:$SHORTHASH" ./backend
+docker build -t "${DockerRepo}simple-fortune-cookie-frontend:latest" -t "${DockerRepo}simple-fortune-cookie-frontend:$SHORTHASH" ./frontend
--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
 SHORTHASH="$(git rev-parse --short HEAD)"
-docker tag $IMAGE_NAME $DOCKER_REPO:$SHORTHASH
-docker push $DOCKER_REPO:$SHORTHASH
-docker push "$docker_username/simple-fortune-cookie-backend:$Tag"
-docker push "$docker_username/simple-fortune-cookie-frontend:$Tag" &
+docker tag "$IMAGE_NAME" "$docker_username/simple-fortune-cookie-backend:$SHORTHASH"
+docker push "$docker_username/simple-fortune-cookie-backend:$SHORTHASH"
+docker push "$docker_username/simple-fortune-cookie-frontend:$SHORTHASH" &
 wait
