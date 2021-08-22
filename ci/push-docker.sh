06bf159435bbcf31cc93edeead4ae8dda14098a4
Samuel Njri <sanjr19@student.sdu.dk>
2021-08-22 15:52:52 +0000

    add files for sha

---
+++ ci/push-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "$docker_password" | docker login --username "$docker_username" --password-stdin
+SHORTHASH="$(git rev-parse --short HEAD)"
+docker tag $IMAGE_NAME $DOCKER_REPO:$SHORTHASH
+docker push $DOCKER_REPO:$SHORTHASH
+docker push "$docker_username/simple-fortune-cookie-app:latest" &
+wait
