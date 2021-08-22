a1b577cefaa3ce67371c4c0e489c85423687d309
Samuel Njri <sanjr19@student.sdu.dk>
2021-08-22 16:07:38 +0000

    add files for sha v1.2

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
 SHORTHASH="$(git rev-parse --short HEAD)"
 docker tag $IMAGE_NAME $DOCKER_REPO:$SHORTHASH
 docker push $DOCKER_REPO:$SHORTHASH
-docker push "$docker_username/simple-fortune-cookie-backend:latest"
-docker push "$docker_username/simple-fortune-cookie-frontend:latest" &
+docker push "$docker_username/simple-fortune-cookie-backend:$Tag"
+docker push "$docker_username/simple-fortune-cookie-frontend:$Tag" &
 wait
